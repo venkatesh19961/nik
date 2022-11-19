e1c78bfda5438d3714be16eb2ea7fa9af9ab8733
venkatesh19961 <118588147+venkatesh19961@users.noreply.github.com>
2022-11-19 11:24:26 +0530

    done

--- github/src/github/newkjauh.java
+++ github/src/github/newkjauh.java
@@ -2,7 +2,7 @@ package github;
 
 public class newkjauh {
 	public static void main(String[] args) {
-		System.out.println("venkatesh hi");
+		System.out.println("venkatesh................................... hi");
 	}
 
 }
